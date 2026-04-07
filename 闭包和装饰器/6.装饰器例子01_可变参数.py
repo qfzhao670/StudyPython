dccc25571a1a4302c65b000b3da21b8064863e8b
qfzhao <2805321430@qq.com>
2026-04-07 13:13:20 +0800

    进行了装饰器，可变参数的练习

---
+++ 闭包和装饰器/6.装饰器例子01_可变参数.py
@@ -0,0 +1,13 @@
+def my_docorator(fn_name):
+    def inner_func(*args, **kwargs):
+        print("正在进行计算中.....")
+        return fn_name(*args, **kwargs)
+    return inner_func
+
+@my_docorator
+def get_sum(*args, **kwargs):
+    print(args)
+    print(kwargs)
+    return sum(args) + sum(kwargs.values())
+
+print(get_sum(1, 2, 3, a=4, b=5))
