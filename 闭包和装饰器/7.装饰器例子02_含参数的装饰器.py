3a020a920539a3d7148835d6e0422840f90a04ba
qfzhao <2805321430@qq.com>
2026-04-07 13:39:48 +0800

    学习了带参数的装饰器

---
+++ 闭包和装饰器/7.装饰器例子02_含参数的装饰器.py
@@ -0,0 +1,30 @@
+"""
+案例：含参数的装饰器
+
+记忆：
+    1.一个装饰器智能携带一个参数
+    2.如果装饰器需要加载多个参数，可以在装饰器外面再包裹一层，把装饰器作为内部函数返回即可。
+"""
+def wrapper(flag1):
+    def my_decorator(fn_name):
+        def inner_func(a, b):
+            if flag1 == "+":
+                print("正在执行【加法】运算")
+            if flag1 == "-":
+                print("正在执行【减法】运算")
+            fn_name(a, b)
+        return inner_func
+    return my_decorator
+
+@wrapper("+")
+def get_sum(a, b):
+    print(f"求和结果为{a + b}")
+
+@wrapper("+")
+def get_sub(a, b):
+    print(f"求和结果为{a - b}")
+
+if __name__ == '__main__':
+    get_sum(1, 2)
+    print("*" * 21)
+    get_sub(1, 2)
